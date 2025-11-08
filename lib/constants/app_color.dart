db4e809f620f32c506f22e58936f7a2fbdbc31e7
Adepoju Opeyemi <opepoju@gmail.com>
2025-11-08 08:11:04 +0100

    feat: add AppColors for easy customization.

---
+++ lib/constants/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xDA6652FE);
+  static const Color gray = Color(0x006C757D);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color success = Color(0xFF48D49E);
+  static const Color error = Color(0xFFFF4D4F);
+}
